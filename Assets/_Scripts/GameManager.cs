cbe6a318ca3edba7ccaf5c5389be1331296bb78e
Bilal <asgharbilal8@gmail.com>
2017-11-12 18:17:34 +0000

    Tried to find a way to make to objects intersect without depending on physic components

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject BladePrefab;
+
+    void Start()
+    {
+        Instantiate(BladePrefab);
+    }
+}
--- Assets/_Scripts/Stick.cs
+++ Assets/_Scripts/Stick.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class Stick : MonoBehaviour
 {
+    public GameObject m_MyObject, m_NewObject;
+    Collider2D m_Collider, m_Collider2;
+
+    void Start()
+    {
+        //Check that the first GameObject exists in the Inspector and fetch the Collider
+        if (m_MyObject != null)
+            m_Collider = m_MyObject.GetComponent<Collider2D>();
+
+        //Check that the second GameObject exists in the Inspector and fetch the Collider
+        if (m_NewObject != null)
+            m_Collider2 = m_NewObject.GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        bool Intersecting = (m_Collider.bounds.Intersects(m_Collider2.bounds));
+        bool Colliderenabled = (m_Collider);
+        bool Collider2enabled = (m_Collider2);
+        //If the first GameObject's Bounds enters the second GameObject's Bounds, output the message
+
+        if (Intersecting)
+        {
+            m_NewObject.SetActive(value: true);
+        }
+
+        if (Intersecting)
+        {
+            Debug.Log("Bounds intersecting");
+            Debug.Log(Intersecting);
+            Destroy(gameObject);
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         bool collidedWithBlade = (collision.CompareTag("Blade"));
         bool collidedWithDeathZone = (collision.CompareTag("DeathZone"));
 
+
         if (collidedWithBlade)
         {
             //ToDo: Instaniate a stick break effect
-            Destroy(gameObject);
         }
 
         else if (collidedWithDeathZone)

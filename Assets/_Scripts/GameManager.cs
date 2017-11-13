ee358b0eef99f43ce7b72ca60e56ffc60495929d
Bilal <asgharbilal8@gmail.com>
2017-11-13 21:00:57 +0000

    Figured out a way of finding the width of the stick

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject BladePrefab;
+    public float Percent;
+
+    private Stick _stick;
 
     void Start()
     {
+        if (_stick != null)
+        {
+            _stick = FindObjectOfType<Stick>();
+        }
+        Percent = _stick.spriterenderer.bounds.size.x;
         Instantiate(BladePrefab);
+        Percent = Random.Range(0, Percent);
+        Debug.Log(Percent/* * 100*/);
     }
 }
--- Assets/_Scripts/Stick.cs
+++ Assets/_Scripts/Stick.cs
@@ -4,22 +4,35 @@ using UnityEngine;
 
 public class Stick : MonoBehaviour
 {
-    public GameObject m_MyObject, m_NewObject;
-    Collider2D m_Collider, m_Collider2;
+    //public GameObject m_MyObject, m_NewObject;
+    //private Collider2D m_Collider, m_Collider2;
+
+    public Vector2 widthOfStick;
+    public SpriteRenderer spriterenderer;
+    public float Percent;
 
     void Start()
     {
+        spriterenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        Percent = spriterenderer.bounds.size.x;
+        /*
         //Check that the first GameObject exists in the Inspector and fetch the Collider
         if (m_MyObject != null)
             m_Collider = m_MyObject.GetComponent<Collider2D>();
 
         //Check that the second GameObject exists in the Inspector and fetch the Collider
         if (m_NewObject != null)
-            m_Collider2 = m_NewObject.GetComponent<Collider2D>();
+            m_Collider2 = m_NewObject.GetComponent<Collider2D>();*/
     }
 
     void Update()
     {
+        //Vector2 spriteWidth = spriterenderer.bounds.size;
+        //Vector2 StickMin = spriterenderer.bounds.min;
+        //Vector2 StickMax = spriterenderer.bounds.max;
+        Percent = Random.Range(0, Percent);
+        Debug.Log(Percent/* * 100*/);
+        /*
         bool Intersecting = (m_Collider.bounds.Intersects(m_Collider2.bounds));
         bool Colliderenabled = (m_Collider);
         bool Collider2enabled = (m_Collider2);
@@ -35,7 +48,7 @@ public class Stick : MonoBehaviour
             Debug.Log("Bounds intersecting");
             Debug.Log(Intersecting);
             Destroy(gameObject);
-        }
+        }*/
     }
 
     void OnTriggerEnter2D(Collider2D collision)
